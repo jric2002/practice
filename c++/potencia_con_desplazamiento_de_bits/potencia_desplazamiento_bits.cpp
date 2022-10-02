4ef2d2bb0c8753267de1bd1969666173663bda72
jric2002 <82920872+jric2002@users.noreply.github.com>
2022-10-02 01:58:39 -0500

    Practice

---
+++ c++/potencia_con_desplazamiento_de_bits/potencia_desplazamiento_bits.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+/* Author: José Rodolfo (jric2002) */
+using namespace std;
+/* Declaration */
+int main() {
+  int n;
+  for (int i = 0; i < 10; i++) {
+    cout << "1 << " << i << " = " << (1 << i) << endl;
+  }
+  return 0;
+}
+/* Definition */
--- python/practica/practica.py
+++ python/practica/practica.py
@@ -1,3 +1,2 @@
 # Author: José Rodolfo (jric2002)
-import os
-print((os.path.basename(__file__)).replace(".py", ""))
+print("hola")
