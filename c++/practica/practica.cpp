1d6c8e82bdec375e826f8bc0481d7116c8713a9f
jric2002 <82920872+jric2002@users.noreply.github.com>
2023-03-17 12:09:10 -0500

    Practice

--- c++/practica/practica.cpp
+++ c++/practica/practica.cpp
@@ -4,22 +4,7 @@
 using namespace std;
 /* Declaration */
 int main() {
-  int n;
-  double x, y;
-  vector<pair<double, double>> datos;
-  datos.push_back(make_pair());
-  cout << "AJUSTE DE CURVAS" << endl;
-  cout.width(10);
-  cout.fill('-');
-  cout << "" << endl;
-  cout << "[+] Regresion lineal simple" << endl;
-  cout << "Ingrese la cantidad de datos: ";
-  cin >> n;
-  for (int i = 0; i < n; i++) {
-    cout << (i + 1) << " dato: ";
-    cin >> x >> y;
-    datos.push_back(make_pair(x, y));
-  }
+  
   return 0;
 }
 /* Definition */
