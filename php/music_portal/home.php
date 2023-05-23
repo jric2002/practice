0a623691ee223cf85e91483f1c61c3d9173c2271
jric2002 <82920872+jric2002@users.noreply.github.com>
2023-05-22 23:56:50 -0500

    Practice

--- php/music_portal/home.php
+++ php/music_portal/home.php
@@ -11,10 +11,24 @@ else {
   exit();
 }
 include_once("./connection.php");
-$query = "SELECT songs.id, songs.title, artists.name, songs.duration FROM songs JOIN artists ON songs.artist = artists.id";
-$r = $connection->query($query);
-$r = $r->fetch_all(MYSQLI_ASSOC);
-print_r($r);
+if (isset($_POST["search"])) {
+  $sn = "%".$_POST["search"]."%";
+  $query = $connection->prepare("SELECT songs.id, songs.title, artists.name, songs.duration, songs.location FROM songs JOIN artists ON songs.artist = artists.id WHERE songs.title LIKE ? OR artists.name LIKE ? LIMIT 10");
+  $query->bind_param("ss", $sn, $sn);
+  $query->execute();
+  $r = $query->get_result();
+  $r = $r->fetch_all(MYSQLI_ASSOC);
+}
+else {
+  $query = "SELECT songs.id, songs.title, artists.name, songs.duration, songs.location FROM songs JOIN artists ON songs.artist = artists.id LIMIT 5";
+  $r = $connection->query($query);
+  $r = $r->fetch_all(MYSQLI_ASSOC);
+}
+// $query = "SELECT s.id, s.title, a.name, c.name, s.duration FROM songs s JOIN artists  a ON s.artist = a.id LEFT JOIN (SELECT c.song, a.name FROM collaborations c JOIN artists a ON c.feat = a.id) c ON c.song = s.id";
+/* foreach ($r as $song) {
+  print_r($song["title"]);
+  echo "<br>";
+} */
 ?>
 <!DOCTYPE html>
   <html lang="es">
@@ -40,30 +54,53 @@ print_r($r);
         <title>Spotify</title>
         <path fill="currentColor" d="M8 171c0 92 76 168 168 168s168-76 168-168S268 4 176 4 8 79 8 171zm230 78c-39-24-89-30-147-17-14 2-16-18-4-20 64-15 118-8 162 19 11 7 0 24-11 18zm17-45c-45-28-114-36-167-20-17 5-23-21-7-25 61-18 136-9 188 23 14 9 0 31-14 22zM80 133c-17 6-28-23-9-30 59-18 159-15 221 22 17 9 1 37-17 27-54-32-144-35-195-19zm379 91c-17 0-33-6-47-20-1 0-1 1-1 1l-16 19c-1 1-1 2 0 3 18 16 40 24 64 24 34 0 55-19 55-47 0-24-15-37-50-46-29-7-34-12-34-22s10-16 23-16 25 5 39 15c0 0 1 1 2 1s1-1 1-1l14-20c1-1 1-1 0-2-16-13-35-20-56-20-31 0-53 19-53 46 0 29 20 38 52 46 28 6 32 12 32 22 0 11-10 17-25 17zm95-77v-13c0-1-1-2-2-2h-26c-1 0-2 1-2 2v147c0 1 1 2 2 2h26c1 0 2-1 2-2v-46c10 11 21 16 36 16 27 0 54-21 54-61s-27-60-54-60c-15 0-26 5-36 17zm30 78c-18 0-31-15-31-35s13-34 31-34 30 14 30 34-12 35-30 35zm68-34c0 34 27 60 62 60s62-27 62-61-26-60-61-60-63 27-63 61zm30-1c0-20 13-34 32-34s33 15 33 35-13 34-32 34-33-15-33-35zm140-58v-29c0-1 0-2-1-2h-26c-1 0-2 1-2 2v29h-13c-1 0-2 1-2 2v22c0 1 1 2 2 2h13v58c0 23 11 35 34 35 9 0 18-2 25-6 1 0 1-1 1-2v-21c0-1 0-2-1-2h-2c-5 3-11 4-16 4-8 0-12-4-12-12v-54h30c1 0 2-1 2-2v-22c0-1-1-2-2-2h-30zm129-3c0-11 4-15 13-15 5 0 10 0 15 2h1s1-1 1-2V93c0-1 0-2-1-2-5-2-12-3-22-3-24 0-36 14-36 39v5h-13c-1 0-2 1-2 2v22c0 1 1 2 2 2h13v89c0 1 1 2 2 2h26c1 0 1-1 1-2v-89h25l37 89c-4 9-8 11-14 11-5 0-10-1-15-4h-1l-1 1-9 19c0 1 0 3 1 3 9 5 17 7 27 7 19 0 30-9 39-33l45-116v-2c0-1-1-1-2-1h-27c-1 0-1 1-1 2l-28 78-30-78c0-1-1-2-2-2h-44v-3zm-83 3c-1 0-2 1-2 2v113c0 1 1 2 2 2h26c1 0 1-1 1-2V134c0-1 0-2-1-2h-26zm-6-33c0 10 9 19 19 19s18-9 18-19-8-18-18-18-19 8-19 18zm245 69c10 0 19-8 19-18s-9-18-19-18-18 8-18 18 8 18 18 18zm0-34c9 0 17 7 17 16s-8 16-17 16-16-7-16-16 7-16 16-16zm4 18c3-1 5-3 5-6 0-4-4-6-8-6h-8v19h4v-6h4l4 6h5zm-3-9c2 0 4 1 4 3s-2 3-4 3h-4v-6h4z"></path>
       </svg>
-      <a href="<?php echo ".".$_SERVER["PHP_SELF"]."?logout";?>">Cerrar sesión</a>
+      <form action=".<?php echo $_SERVER["PHP_SELF"]?>" method="post">
+        <input type="search" name="search" class="search" id="search" placeholder="Buscar"/>
+        <input type="submit" value="submit"/>
+      </form>
+      <div>
+        <h1 class="user"><?php echo $_COOKIE["user"];?></h1>
+        <a href="<?php echo ".".$_SERVER["PHP_SELF"]."?logout";?>">Cerrar sesión</a>
+      </div>
     </header>
     <section class="songs">
+      <?php foreach ($r as $song):?>
       <div class="card">
-        <!-- Cover -->
-        <img src="./img/cover.jpg" class="cover" alt="Cover"/>
-        <div class="data">
-          <!-- Title -->
-          <h1 class="title" id="title">title</h1>
-          <!-- Author -->
-          <h4 class="author" id="author">artist</h3>
+        <div class="metadata">
+          <img src="./songs/<?php echo $song["location"]."/cover.jpg"?>" class="cover" alt="Cover"/>
+          <div class="data">
+            <!-- Title -->
+            <h1 class="title" id="title"><?php echo $song["title"];?></h1>
+            <!-- Author -->
+            <h4 class="author" id="author"><?php echo $song["name"];?></h3>
+            <p class="duration"><?php echo $song["duration"];?></p>
+          </div>
         </div>
+        <div class="play-song" name="<?php
+          $song_path = glob("./songs/".$song["location"]."/*.mp3");
+          $song_name = basename($song_path[0]);
+          echo $song_name;
+        ;?>" id="<?php
+          echo $song["location"];
+        ?>">
+          <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="play" class="svg-inline--fa fa-play fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+            <path fill="currentColor" d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z"></path>
+          </svg>
+        </div>
+        <!-- Cover -->
       </div>
+      <?php endforeach;?>
     </section>
     <!-- Media player -->
     <div class="media-player" id="media-player">
       <div class="info">
         <!-- Cover -->
-        <img src="./img/cover.jpg" class="cover" alt="Cover"/>
+        <img src="./img/cover-template.png" class="cover" id="cover" alt="Cover"/>
         <div class="data">
           <!-- Title -->
-          <h1 class="title" id="title">Title</h1>
+          <h1 class="song-name" id="song-name">Song</h1>
           <!-- Author -->
-          <h4 class="author" id="author">Author</h3>
+          <h4 class="artist" id="artist">Artist</h3>
         </div>
       </div>
       <div class="controls-general">
@@ -71,12 +108,16 @@ print_r($r);
         <div class="track" id="track">
           <div class="progress" id="progress"></div>
         </div>
+        <div class="duration">
+          <p class="current-time" id="current-time">00:00:00</p>
+          <p class="end-time" id="end-time">00:00:00</p>
+        </div>
         <!-- Controls -->
         <div class="controls">
           <div class="previous" id="previous">
-          <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="backward" class="svg-inline--fa fa-backward fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-            <path fill="currentColor" d="M11.5 280.6l192 160c20.6 17.2 52.5 2.8 52.5-24.6V96c0-27.4-31.9-41.8-52.5-24.6l-192 160c-15.3 12.8-15.3 36.4 0 49.2zm256 0l192 160c20.6 17.2 52.5 2.8 52.5-24.6V96c0-27.4-31.9-41.8-52.5-24.6l-192 160c-15.3 12.8-15.3 36.4 0 49.2z"></path>
-          </svg>
+            <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="backward" class="svg-inline--fa fa-backward fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+              <path fill="currentColor" d="M11.5 280.6l192 160c20.6 17.2 52.5 2.8 52.5-24.6V96c0-27.4-31.9-41.8-52.5-24.6l-192 160c-15.3 12.8-15.3 36.4 0 49.2zm256 0l192 160c20.6 17.2 52.5 2.8 52.5-24.6V96c0-27.4-31.9-41.8-52.5-24.6l-192 160c-15.3 12.8-15.3 36.4 0 49.2z"></path>
+            </svg>
           </div>
           <div class="play" id="play">
             <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="play" class="svg-inline--fa fa-play fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
@@ -89,10 +130,14 @@ print_r($r);
             </svg>
           </div>
           <div class="next" id="next">
-          <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="forward" class="svg-inline--fa fa-forward fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-            <path fill="currentColor" d="M500.5 231.4l-192-160C287.9 54.3 256 68.6 256 96v320c0 27.4 31.9 41.8 52.5 24.6l192-160c15.3-12.8 15.3-36.4 0-49.2zm-256 0l-192-160C31.9 54.3 0 68.6 0 96v320c0 27.4 31.9 41.8 52.5 24.6l192-160c15.3-12.8 15.3-36.4 0-49.2z"></path>
-          </svg>
+            <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="forward" class="svg-inline--fa fa-forward fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+              <path fill="currentColor" d="M500.5 231.4l-192-160C287.9 54.3 256 68.6 256 96v320c0 27.4 31.9 41.8 52.5 24.6l192-160c15.3-12.8 15.3-36.4 0-49.2zm-256 0l-192-160C31.9 54.3 0 68.6 0 96v320c0 27.4 31.9 41.8 52.5 24.6l192-160c15.3-12.8 15.3-36.4 0-49.2z"></path>
+            </svg>
           </div>
+        </div>
+        <!-- Volume -->
+        <div class="volume">
+          &nbsp;<input type="range" name="volume" id="volume" min="0" max="1" step="0.01"/>
         </div>
       </div>
     </div>
--- php/music_portal/js/home.js
+++ php/music_portal/js/home.js
@@ -1,25 +1,16 @@
+const PROTOCOL = window.location.protocol;
+const HOSTNAME = window.location.hostname;
+const PORT = (window.location.port == '') ? window.location.port : ":" + window.location.port;
+const URL = PROTOCOL + "//" + HOSTNAME + PORT;
 /* Media player */
-/** Playlist **/
-const music = [
-  {
-    path: "./../music/Of Monsters And Men  Little Talks Official Video.mp3",
-    title: "Little talks",
-    album: "",
-    genre: "",
-    author: "Of Monsters And Men"
-  },
-  {
-    path: "./../music/Of Monsters And Men  Mountain Sound Official.mp3",
-    title: "Mountain Sound",
-    album: "",
-    genre: "",
-    author: "Of Monsters And Men"
-  }
-];
-/** Title **/
-const TITLE = document.getElementById("title");
-/** Author **/
-const AUTHOR = document.getElementById("author");
+/** Cover **/
+const COVER = document.getElementById("cover");
+/** Song name **/
+const SONG_NAME = document.getElementById("song-name");
+/** Artist **/
+const ARTIST = document.getElementById("artist");
+/** Play song **/
+const PLAY_SONG = document.querySelectorAll(".play-song");
 /** Controls **/
 const PREVIOUS = document.getElementById("previous");
 const PLAY = document.getElementById("play");
@@ -27,63 +18,65 @@ const PAUSE = document.getElementById("pause");
 const NEXT = document.getElementById("next");
 const TRACK = document.getElementById("track");
 const PROGRESS = document.getElementById("progress");
+const CURRENT_TIME = document.getElementById("current-time");
+const END_TIME = document.getElementById("end-time");
+const VOLUME = document.getElementById("volume");
 var id_music = 0;
-var audio = new Audio(music[id_music].path);
-TITLE.innerHTML = music[id_music].title;
-AUTHOR.innerHTML = music[id_music].author;
-PLAY.addEventListener("click", function() {
+var audio = new Audio();
+function playSong() {
   audio.play()
   PLAY.classList.add("hide-button");
   PAUSE.classList.remove("hide-button");
-});
+}
+PLAY.addEventListener("click", playSong);
 PAUSE.classList.add("hide-button");
 PAUSE.addEventListener("click", function() {
   PLAY.classList.remove("hide-button");
   PAUSE.classList.add("hide-button");
   audio.pause();
 });
-PREVIOUS.addEventListener("click", function() {
-  audio.pause();
-  audio.currentTime = audio.duration;
-  id_music = (id_music == 0) ? 0 : (id_music - 1);
-  audio = new Audio(music[id_music].path);
-  audio.play()
-  .then(function() {
-    TITLE.innerHTML = music[id_music].title;
-    AUTHOR.innerHTML = music[id_music].author;
-    PLAY.classList.add("hide-button");
-    PAUSE.classList.remove("hide-button");
-    progress(audio, TRACK, PROGRESS);
-  })
-  .catch(function(error) {
-    console.log(error);
-  });
-  audio.addEventListener("ended", function() {
-    PLAY.classList.remove("hide-button");
-    PAUSE.classList.add("hide-button");
-  });
-});
 NEXT.addEventListener("click", function() {
-  audio.pause();
-  audio.currentTime = audio.duration;
-  id_music = (id_music == (music.length - 1)) ? (music.length - 1) : (id_music + 1);
-  audio = new Audio(music[id_music].path);
-  audio.play()
-  .then(function() {
-    TITLE.innerHTML = music[id_music].title;
-    AUTHOR.innerHTML = music[id_music].author;
-    PLAY.classList.add("hide-button");
-    PAUSE.classList.remove("hide-button");
-    progress(audio, TRACK, PROGRESS);
-  })
-  .catch(function(error) {
-    console.log(error);
-  });
-  audio.addEventListener("ended", function() {
-    PLAY.classList.remove("hide-button");
-    PAUSE.classList.add("hide-button");
-  });
+  audio.currentTime += 5
 });
+PREVIOUS.addEventListener("click", function() {
+  audio.currentTime -= 5
+});
+function showButtonVolume(vol) {
+  let vol_par = VOLUME.parentElement;
+  vol = vol * 100;
+  if (vol >= 75) {
+    vol_par.insertAdjacentHTML("afterbegin", "<svg id=\"volume-1\" aria-hidden=\"true\" focusable=\"false\" data-prefix=\"fas\" data-icon=\"volume-up\" class=\"svg-inline--fa fa-volume-up fa-w-18\" role=\"img\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 576 512\"><path fill=\"currentColor\" d=\"M215.03 71.05L126.06 160H24c-13.26 0-24 10.74-24 24v144c0 13.25 10.74 24 24 24h102.06l88.97 88.95c15.03 15.03 40.97 4.47 40.97-16.97V88.02c0-21.46-25.96-31.98-40.97-16.97zm233.32-51.08c-11.17-7.33-26.18-4.24-33.51 6.95-7.34 11.17-4.22 26.18 6.95 33.51 66.27 43.49 105.82 116.6 105.82 195.58 0 78.98-39.55 152.09-105.82 195.58-11.17 7.32-14.29 22.34-6.95 33.5 7.04 10.71 21.93 14.56 33.51 6.95C528.27 439.58 576 351.33 576 256S528.27 72.43 448.35 19.97zM480 256c0-63.53-32.06-121.94-85.77-156.24-11.19-7.14-26.03-3.82-33.12 7.46s-3.78 26.21 7.41 33.36C408.27 165.97 432 209.11 432 256s-23.73 90.03-63.48 115.42c-11.19 7.14-14.5 22.07-7.41 33.36 6.51 10.36 21.12 15.14 33.12 7.46C447.94 377.94 480 319.54 480 256zm-141.77-76.87c-11.58-6.33-26.19-2.16-32.61 9.45-6.39 11.61-2.16 26.2 9.45 32.61C327.98 228.28 336 241.63 336 256c0 14.38-8.02 27.72-20.92 34.81-11.61 6.41-15.84 21-9.45 32.61 6.43 11.66 21.05 15.8 32.61 9.45 28.23-15.55 45.77-45 45.77-76.88s-17.54-61.32-45.78-76.86z\"></path></svg>");
+  }
+  else if (vol >= 25) {
+    vol_par.insertAdjacentHTML("afterbegin", "<svg id=\"volume-2\" aria-hidden=\"true\" focusable=\"false\" data-prefix=\"fas\" data-icon=\"volume-down\" class=\"svg-inline--fa fa-volume-down fa-w-12\" role=\"img\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 384 512\"> <path fill=\"currentColor\" d=\"M215.03 72.04L126.06 161H24c-13.26 0-24 10.74-24 24v144c0 13.25 10.74 24 24 24h102.06l88.97 88.95c15.03 15.03 40.97 4.47 40.97-16.97V89.02c0-21.47-25.96-31.98-40.97-16.98zm123.2 108.08c-11.58-6.33-26.19-2.16-32.61 9.45-6.39 11.61-2.16 26.2 9.45 32.61C327.98 229.28 336 242.62 336 257c0 14.38-8.02 27.72-20.92 34.81-11.61 6.41-15.84 21-9.45 32.61 6.43 11.66 21.05 15.8 32.61 9.45 28.23-15.55 45.77-45 45.77-76.88s-17.54-61.32-45.78-76.87z\"></path> </svg>");
+  }
+  else if (vol >= 5) {
+    vol_par.insertAdjacentHTML("afterbegin", "<svg id=\"volume-3\" aria-hidden=\"true\" focusable=\"false\" data-prefix=\"fas\" data-icon=\"volume-off\" class=\"svg-inline--fa fa-volume-off fa-w-8\" role=\"img\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 256 512\"> <path fill=\"currentColor\" d=\"M215 71l-89 89H24a24 24 0 0 0-24 24v144a24 24 0 0 0 24 24h102.06L215 441c15 15 41 4.47 41-17V88c0-21.47-26-32-41-17z\"></path> </svg>");
+  }
+  else {
+    vol_par.insertAdjacentHTML("afterbegin", "<svg id=\"volume-4\" aria-hidden=\"true\" focusable=\"false\" data-prefix=\"fas\" data-icon=\"volume-mute\" class=\"svg-inline--fa fa-volume-mute fa-w-16\" role=\"img\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 512 512\"> <path fill=\"currentColor\" d=\"M215.03 71.05L126.06 160H24c-13.26 0-24 10.74-24 24v144c0 13.25 10.74 24 24 24h102.06l88.97 88.95c15.03 15.03 40.97 4.47 40.97-16.97V88.02c0-21.46-25.96-31.98-40.97-16.97zM461.64 256l45.64-45.64c6.3-6.3 6.3-16.52 0-22.82l-22.82-22.82c-6.3-6.3-16.52-6.3-22.82 0L416 210.36l-45.64-45.64c-6.3-6.3-16.52-6.3-22.82 0l-22.82 22.82c-6.3 6.3-6.3 16.52 0 22.82L370.36 256l-45.63 45.63c-6.3 6.3-6.3 16.52 0 22.82l22.82 22.82c6.3 6.3 16.52 6.3 22.82 0L416 301.64l45.64 45.64c6.3 6.3 16.52 6.3 22.82 0l22.82-22.82c6.3-6.3 6.3-16.52 0-22.82L461.64 256z\"></path> </svg>");
+  }
+}
+showButtonVolume(parseFloat(VOLUME.value));
+VOLUME.addEventListener("input", (event) => {
+  audio.volume = event.target.value;
+  let svg = (VOLUME.parentElement).getElementsByTagName("svg");
+  svg[0].remove();
+  showButtonVolume(event.target.value);
+});
+/** Get Time **/
+function getTime(seconds) {
+  let s = Math.ceil(seconds);
+  let h = parseInt(s/3600);
+  s = s % 3600;
+  let m = parseInt(s/60);
+  s = s % 60;
+  h = (h < 10) ? "0" + h.toString() : h;
+  m = (m < 10) ? "0" + m.toString() : m;
+  s = (s < 10) ? "0" + s.toString() : s;
+  let time = [h, m, s];
+  return time.join(":");
+}
 /** Track **/
 function progress(audio, TRACK, PROGRESS) {
   TRACK.addEventListener("mousedown", function(event) {
@@ -93,12 +86,27 @@ function progress(audio, TRACK, PROGRESS) {
     //PROGRESS.style.width = ((x_track * 100) / TRACK.offsetWidth).toString() + "%";
   });
   audio.addEventListener("timeupdate", function() {
+    CURRENT_TIME.innerHTML = getTime(audio.currentTime);
     PROGRESS.style.width = ((audio.currentTime * 100) / audio.duration).toString() + "%";
     //console.log(PROGRESS.style.width);
   });
 }
 audio.addEventListener("ended", function() {
-  PLAY.classList.remove("hide-button");
   PAUSE.classList.add("hide-button");
+  PLAY.classList.remove("hide-button");
 });
-progress(audio, TRACK, PROGRESS);
+PLAY_SONG.forEach(ps => {
+  ps.addEventListener("click", function(event) {
+    audio.pause();
+    let song_path = URL + "/songs/" + ps.getAttribute("id") + "/" + ps.getAttribute("name");
+    // console.log(encodeURI(song_path));
+    audio = new Audio(encodeURI(song_path));
+    playSong();
+    progress(audio, TRACK, PROGRESS);
+    data = (((ps.parentElement.children)[0].children)[1].children);
+    END_TIME.innerHTML = data[2].textContent;
+    COVER.src = "./songs/" + ps.getAttribute("id") + "/cover.jpg";
+    SONG_NAME.innerHTML = data[0].textContent;
+    ARTIST.innerHTML = data[1].textContent;
+  });
+});
